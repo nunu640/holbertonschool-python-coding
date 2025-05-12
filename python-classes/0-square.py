cc9f23e715cdddcaee6a97e86f39e0f424d4ce23
nunu640 <nuranea640@gmail.com>
2025-05-12 08:35:16 +0000

    task0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Defines a class Square"""
-
-
 class Square:
     """Represents a square"""
-
     def __init__(self, size):
         """Initialize the square with a private size"""
         self.__size = size
